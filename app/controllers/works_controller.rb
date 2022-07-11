39b5e21fb7dca093f02bb6e1456cff8c77724e74
yuchan1120 <nogginpanic@gmail.com>
2022-07-11 22:19:19 +0900

    業務登録機能を追加

--- app/controllers/works_controller.rb
+++ app/controllers/works_controller.rb
@@ -4,4 +4,22 @@ class WorksController < ApplicationController
   def index
     @works = Work.where("user_id::text LIKE?", "#{current_user.id}")
   end
+
+  def new
+    @work = Work.new
+  end
+
+  def create
+    @work = Work.new(work_params)
+    if @work.save
+      flash[:notice] = "業務を新規登録しました"
+      redirect_to :works
+    else
+      render "new"
+    end
+  end
+
+  def work_params
+    params.require(:work).permit(:name, :time_required, :user_id, :taxon_id)
+  end
 end
