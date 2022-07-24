a827f62fb4fafc33bc45f41483f1152bfa6e06b6
yuchan1120 <nogginpanic@gmail.com>
2022-07-24 15:38:40 +0900

    モデル同士の関連性を修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
+  has_many :tables
+  has_many :works
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
--- app/models/work.rb
+++ app/models/work.rb
@@ -1,5 +1,6 @@
 class Work < ApplicationRecord
   belongs_to :taxon
+  belongs_to :user
   validates :name, presence: true
   validates :time_required, numericality: true, presence: true
   validates :user_id, numericality: true, presence: true
