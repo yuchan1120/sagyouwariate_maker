dd0af270463d76d0f6a06932782989a0f0ad4f5f
yuchan1120 <nogginpanic@gmail.com>
2022-07-26 00:24:51 +0900

    Tablesコントローラのcreateアクションに関するテストを追加

--- spec/requests/tables_spec.rb
+++ spec/requests/tables_spec.rb
@@ -46,4 +46,34 @@ RSpec.describe "Tables", type: :request do
       #追加予定
     end
   end
+
+  describe"#create"do
+    context "as an authorized user" do
+      before do
+        @user = FactoryBot.create(:user)
+      end
+
+      it "adds a table" do
+        table_params = FactoryBot.attributes_for(:table, user_id: @user.id)
+        sign_in @user
+        expect {
+          post tables_path, params: { table: table_params }
+        }.to change(@user.tables, :count).by(1)
+      end
+    end
+
+    context "as a guest" do
+      it "returns a 302 response" do
+        table_params = FactoryBot.attributes_for(:table)
+        post tables_path, params: { table: table_params }
+        expect(response).to have_http_status "302"
+      end
+
+      it "redirects to the sign-in page" do
+        table_params = FactoryBot.attributes_for(:table)
+        post tables_path, params: { table: table_params }
+        expect(response).to redirect_to "/users/sign_in"
+      end
+    end
+  end
 end
