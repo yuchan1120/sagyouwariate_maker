e2d3af284090786dd05e3c7448677ede6edb486f
yuchan1120 <nogginpanic@gmail.com>
2022-08-01 18:23:37 +0900

    コードを並び替え(spec/requests/tables_spec.rb)

--- spec/requests/tables_spec.rb
+++ spec/requests/tables_spec.rb
@@ -30,36 +30,6 @@ RSpec.describe "Tables", type: :request do
     end
   end
 
-  describe "#show" do
-    context "as an authorized user" do
-      it "responds successfully" do
-        sign_in @user
-        get tables_path(:id), params: { id: @table.id }
-        expect(response).to be_successful
-      end
-    end
-
-    context "as an unauthorized user" do
-      it "redirects the page to root_path" do
-        sign_in @another_user
-        get table_path(@table.id)
-        expect(response).to redirect_to root_path
-      end
-    end
-
-    context "as a guest" do
-      it "returns a 302 response" do
-        get table_path(@table.id)
-        expect(response).to have_http_status "302"
-      end
-
-      it "redirects to the sign-in page" do
-        get table_path(@table.id)
-        expect(response).to redirect_to "/users/sign_in"
-      end
-    end
-  end
-
   describe "#new" do
     context "as an authorized user" do
       it "responds successfully" do
@@ -103,6 +73,36 @@ RSpec.describe "Tables", type: :request do
       it "redirects to the sign-in page" do
         table_params = FactoryBot.attributes_for(:table)
         post tables_path, params: { table: table_params }
+        expect(response).to redirect_to "/users/sign_in"
+      end
+    end
+  end
+
+  describe "#show" do
+    context "as an authorized user" do
+      it "responds successfully" do
+        sign_in @user
+        get tables_path(:id), params: { id: @table.id }
+        expect(response).to be_successful
+      end
+    end
+
+    context "as an unauthorized user" do
+      it "redirects the page to root_path" do
+        sign_in @another_user
+        get table_path(@table.id)
+        expect(response).to redirect_to root_path
+      end
+    end
+
+    context "as a guest" do
+      it "returns a 302 response" do
+        get table_path(@table.id)
+        expect(response).to have_http_status "302"
+      end
+
+      it "redirects to the sign-in page" do
+        get table_path(@table.id)
         expect(response).to redirect_to "/users/sign_in"
       end
     end
@@ -201,6 +201,10 @@ RSpec.describe "Tables", type: :request do
     end
 
     context "as a guest" do
+      it "does not delete the table" do
+        expect {delete table_path(@table.id)}.to_not change(Table, :count)
+      end
+
       it "returns a 302 response" do
         delete table_path(@table.id)
         expect(response).to have_http_status "302"
@@ -209,10 +213,6 @@ RSpec.describe "Tables", type: :request do
       it "redirects to the sign-in page" do
         delete table_path(@table.id)
         expect(response).to redirect_to "/users/sign_in"
-      end
-
-      it "does not delete the table" do
-        expect {delete table_path(@table.id)}.to_not change(Table, :count)
       end
     end
   end
