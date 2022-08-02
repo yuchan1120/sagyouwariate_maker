ef482951ce5e3decbf0c175ec2e6a8526378f9a5
yuchan1120 <nogginpanic@gmail.com>
2022-08-02 13:09:32 +0900

    ファクトリでのidを指定するコードを削除

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,12 +1,10 @@
 FactoryBot.define do
   factory :user do
-    id {"1"}
     email { "itirou@example.com" }
     password { "itirou" }
   end
 
   factory :another_user, class: User do
-    id {"2"}
     email { "jirou@example.com" }
     password { "@jirou" }
   end
