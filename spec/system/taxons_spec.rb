fc00795b27d7abd7e447dbbfd81abdaecc4d2269
yuchan1120 <nogginpanic@gmail.com>
2022-08-14 23:45:19 +0900

    カテゴリごとの表示についてのテストを追加

---
+++ spec/system/taxons_spec.rb
@@ -0,0 +1,18 @@
+require 'rails_helper'
+
+RSpec.describe "Taxons", type: :system do
+  before do
+    @user = FactoryBot.create(:user)
+    @taxon = FactoryBot.create(:taxon)
+    @work = FactoryBot.create(:work, user: @user, taxon: @taxon)
+    sign_in_as @user
+    visit works_path
+  end
+
+  scenario "user sees works in taxon" do
+    find("#taxon_link_#{@taxon.id}").click
+
+    expect(current_path).to eq taxon_path(@taxon.id)
+    expect(page).to have_content @work.name
+  end
+end
