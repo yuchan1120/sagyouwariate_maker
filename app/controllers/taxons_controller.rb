95706d57c764c4f695f3364fb06e54c32323e551
yuchan1120 <nogginpanic@gmail.com>
2022-08-24 19:46:40 +0900

    カテゴリ別の業務一覧も新しく更新したものから順に上から表示されるよう修正

--- app/controllers/taxons_controller.rb
+++ app/controllers/taxons_controller.rb
@@ -4,6 +4,6 @@ class TaxonsController < ApplicationController
   def show
     @taxon = Taxon.find(params[:id])
     @all_taxons = Taxon.all
-    @works = Work.where(['user_id::text LIKE? AND taxon_id::text LIKE?', "#{current_user.id}", "#{@taxon.id}"])
+    @works = Work.where(['user_id::text LIKE? AND taxon_id::text LIKE?', "#{current_user.id}", "#{@taxon.id}"]).order(updated_at: "DESC")
   end
 end
