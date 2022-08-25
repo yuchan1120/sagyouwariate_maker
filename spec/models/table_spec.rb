d5bcc4cea9a6d708e0fcf701b827917f1cd68116
yuchan1120 <nogginpanic@gmail.com>
2022-08-25 15:41:28 +0900

    スコープに関するテストを追加

--- spec/models/table_spec.rb
+++ spec/models/table_spec.rb
@@ -3,6 +3,7 @@ require 'rails_helper'
 RSpec.describe Table, type: :model do
   before do
     @user = FactoryBot.create(:user)
+    @another_user = FactoryBot.create(:another_user)
   end
 
   it 'is valid with an user' do
@@ -23,5 +24,24 @@ RSpec.describe Table, type: :model do
     table = Table.new(user_id: 'a')
     table.valid?
     expect(table.errors).to be_of_kind(:user_id, :not_a_number)
+  end
+
+  it 'returns tables with matching owner' do
+    @table1 = FactoryBot.create(:table, user: @user)
+    @table2 = FactoryBot.create(:table, user: @another_user)
+
+    expect(Table.owner(@user)).to include(@table1)
+    expect(Table.owner(@user)).to_not include(@table2)
+    expect(Table.owner(@another_user)).to include(@table2)
+    expect(Table.owner(@another_user)).to_not include(@table1)
+  end
+
+  it 'returns tables that match the search term' do
+    @table1 = FactoryBot.create(:table, title: '忙しい月曜日の作業割当', user: @user)
+    @table2 = FactoryBot.create(:table, title: '暇な月曜日の作業割当', user: @user)
+
+    expect(Table.search_results('月曜日')).to include(@table1, @table2)
+    expect(Table.search_results('忙しい')).to include(@table1)
+    expect(Table.search_results('忙しい')).to_not include(@table2)
   end
 end
