c111e83a572a071fe330c75973088ef1aa9f9d9d
yuchan1120 <nogginpanic@gmail.com>
2022-08-26 16:03:24 +0900

    外部キーのバリデーションを削除

--- app/models/work.rb
+++ app/models/work.rb
@@ -3,8 +3,6 @@ class Work < ApplicationRecord
   belongs_to :user
   validates :name, presence: true
   validates :time_required, numericality: true, presence: true
-  validates :user_id, numericality: true, presence: true
-  validates :taxon_id, numericality: true, presence: true
   scope :category, ->(id) { where('taxon_id::text like?', "#{id}") }
   scope :search_results, ->(keyword) { where('name like?', "%#{keyword}%") }
 end
