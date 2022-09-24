49fa5b3ed87445aee0257fa448769829f6661519
yuchan1120 <nogginpanic@gmail.com>
2022-09-24 22:48:12 +0900

    [fix] create_default_worksメソッドを修正

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_09_24_131225) do
+ActiveRecord::Schema.define(version: 2022_09_24_134021) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -433,6 +433,7 @@ ActiveRecord::Schema.define(version: 2022_09_24_131225) do
     t.integer "taxon_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.boolean "default_data", default: false, null: false
   end
 
 end
--- spec/system/users/registrations_spec.rb
+++ spec/system/users/registrations_spec.rb
@@ -6,6 +6,7 @@ RSpec.describe 'Users::Registrations', type: :system do
     @taxon = create(:regular_work)
     janitor = create(:janitor)
     create(:sample_table, user: janitor)
+    create(:sample_work, user: janitor, taxon: @taxon)
   end
 
   scenario 'user registers a new account' do
@@ -24,7 +25,7 @@ RSpec.describe 'Users::Registrations', type: :system do
 
       find('#header_menu_work_index').click
 
-      expect(page).to have_content '仮点検'
+      expect(page).to have_content 'サンプルワーク'
     end.to change(User, :count).by(1)
   end
 
