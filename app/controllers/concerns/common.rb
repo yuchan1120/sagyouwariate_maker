fa5a2ea37bd431dc599eb0bd2af81deff1a57c11
yuchan1120 <nogginpanic@gmail.com>
2022-09-24 16:08:42 +0900

    [fix] create_default_worksメソッドを修正

--- app/controllers/concerns/common.rb
+++ app/controllers/concerns/common.rb
@@ -5,4 +5,13 @@ module Common
     table.save
     table.update(user_id: current_user.id, default_data: false)
   end
+
+  def create_default_works
+    sample_works = Work.where(default_data: true)
+    sample_works.each do |sample_work|
+      work = sample_work.dup
+      work.save
+      work.update(user_id: current_user.id, default_data: false)
+    end
+  end
 end
